4f7acfefc2715296f8ffe1dcbb95bd576e9f4e83
YashWadhvani <yashwadhvani123@gmail.com>
2025-10-16 00:56:33 +0530

    Backup

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ app.use('/api/police', policeStationRoutes);
 
 app.get("/", (req,res)=> res.send("SafeRoute Backend OK"));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`🚀 Server running on ${PORT}`));
+app.listen(PORT,'0.0.0.0', ()=> console.log(`🚀 Server running on ${PORT}`));
