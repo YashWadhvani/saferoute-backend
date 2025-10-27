7d62771070fb4568eb46e08aae34045ddc65a2a6
YashWadhvani <yashwadhvani123@gmail.com>
2025-10-28 01:06:17 +0530

    Updated for Resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,25 @@
-// Resend-only email sender. Nodemailer has been removed because SMTP may be blocked
-// on some PaaS providers (Render). This module now requires RESEND_API_KEY to be
-// set in the environment. It exposes sendEmail(to, subject, text, html).
-
+// utils/sendEmail.js
 let resendClient;
 try {
-  // require the official resend package
-  const Resend = require('resend').default || require('resend');
-  if (process.env.RESEND_API_KEY) {
+  const { Resend } = require('resend');
+  if (process.env.RESEND_API_KEY)
     resendClient = new Resend(process.env.RESEND_API_KEY);
-    // optional: test that client exists
-    // console.log('Resend client initialized');
-  }
-} catch (err) {
-  // If the package isn't installed, we'll surface an error when sendEmail is called
+} catch (e) {
   resendClient = null;
 }
 
 async function sendEmail(to, subject, text, html) {
-  if (!resendClient) {
-    throw new Error('Resend client not configured. Set RESEND_API_KEY and install the `resend` package.');
-  }
-  const from = process.env.RESEND_FROM || process.env.EMAIL_USER;
-  // Resend expects an object { from, to, subject, text, html }
-  return await resendClient.emails.send({ from, to, subject, text, html });
+  if (!resendClient)
+    throw new Error('Resend not configured. Set RESEND_API_KEY and RESEND_FROM.');
+
+  const from = process.env.RESEND_FROM;
+  return await resendClient.emails.send({
+    from,
+    to,
+    subject,
+    text,
+    html,
+  });
 }
 
 module.exports = sendEmail;
